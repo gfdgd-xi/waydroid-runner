5711d10e8c30eb6bf4352cba470060e1930c3192
gfdgd_xi <3025613752@qq.com>
2023-05-27 21:58:42 +0800

    修改时间

--- mainwindow.py
+++ mainwindow.py
@@ -105,7 +105,7 @@ about = f'''<p align="center"><img width=256 src="{iconPath}"/></p>
 <p>程序官网：{programUrl}</p>
 <p>系统版本：{SystemVersion}</p>
 <p>安装包构建时间：{information['Time']}</p>
-<h1>©2021-{time.strftime("%Y")}</h1>'''
+<h1>©2023-{time.strftime("%Y")}</h1>'''
 tips = ""
 contribute = ""
 iconPath = f"{programPath}"
