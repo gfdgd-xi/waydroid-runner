88fcab91c86e7bf12137b349f88245c6bd3d9974
mozixun <11059267+LFRon@user.noreply.gitee.com>
2023-07-08 13:57:51 +0000

    删除文件 Runner_tools/Magisk_Installer/MagiskCheck.py

--- Runner_tools/Magisk_Installer/MagiskCheck.py
+++
@@ -1,4 +0,0 @@
-import os
-if os.path.exists('/var/lib/waydroid/overlay/system/etc/init/magisk') == True and os.path.exists('/var/lib/waydroid/overlay/system/etc/init/magisk') == True:
-    print('Magisk-Delta已安装')
-else:print('未安装/完全安装Magisk')
