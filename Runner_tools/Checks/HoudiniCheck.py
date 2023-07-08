7c6624f1f2a58a6c3fbbfe806c01f818b4eb6be9
mozixun <11059267+LFRon@user.noreply.gitee.com>
2023-07-08 12:59:29 +0000

    检查Libhoudini是否正确安装脚本 不需要sudo执行
    Signed-off-by: mozixun <11059267+LFRon@user.noreply.gitee.com>

---
+++ Runner_tools/Checks/HoudiniCheck.py
@@ -0,0 +1,5 @@
+import sys
+import os
+if os.path.exists('/var/lib/waydroid/overlay/system/lib/libhoudini.so') == True and os.path.exists('/var/lib/waydroid/overlay/system/lib/arm') == True and os.path.exists('/var/lib/waydroid/overlay/system/lib64/arm64') == True and os.path.exists('/var/lib/waydroid/overlay/system/etc/init/init-houdini.rc') == True:
+    sys.exit(0)
+sys.exit(1)
