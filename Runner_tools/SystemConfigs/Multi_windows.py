cbd7e9637fb83e654facbc77f51ee089c92b31f2
mozixun <11059267+LFRon@user.noreply.gitee.com>
2023-07-08 13:48:35 +0000

    开启多窗口模式脚本 注意!注意!必须在waydroid的session开着时再跑!
    Signed-off-by: mozixun <11059267+LFRon@user.noreply.gitee.com>

---
+++ Runner_tools/SystemConfigs/Multi_windows.py
@@ -0,0 +1,19 @@
+#Please start waydroid session before running this Script!
+import sys
+import time
+import os
+a = os.popen('waydroid status').readlines()
+if a[0].find('STOPPED')!=-1:
+    print('-请先启动容器服务!')
+    print('-程序出现异常,即将退出')
+    sys.exit(1)
+os.popen('waydroid session start')
+time.sleep(40)
+print('正在设置多窗口模式:',end='')
+if os.system('waydroid prop set persist.waydroid.multi_windows true')!=0:
+    print('失败!\n程序即将退出!')
+    sys.exit(1)
+print('成功!')
+sys.exit(0)
+
+
