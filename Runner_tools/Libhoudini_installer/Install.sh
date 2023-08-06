8e49a7019b408faf7c545d0a1742641b0a64518a
mozixun <ronforever@outlook.com>
2023-08-06 10:37:43 +0000

    删除文件 Runner_tools/Libhoudini_installer/Install.sh

--- Runner_tools/Libhoudini_installer/Install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-aria2c -x 16 -s 16 https://jihulab.com/gfdgd-xi/waydroid-image/-/raw/main/houdini-install.tar.gz -d /tmp
-cd /tmp
-tar -xvf houdini-install.tar.gz
-cd houdini-install
-python3 houdini-install.py
-sudo python3 houdini-prop.py
-rm -rf ../houdini-install
-rm ../houdini-install.tar.gz
