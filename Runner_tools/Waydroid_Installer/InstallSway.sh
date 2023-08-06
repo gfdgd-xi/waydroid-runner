33eaa45db1c059cedcab704f12b0a24c5608ac21
gfdgd_xi <3025613752@qq.com>
2023-08-06 22:18:19 +0800

    漏了点东东

--- Runner_tools/Waydroid_Installer/InstallSway.sh
+++ Runner_tools/Waydroid_Installer/InstallSway.sh
@@ -6,7 +6,7 @@ if [[ $? == 0 ]]; then
     sudo apt update
     sudo apt install sway aria2 -y
     # 这个包还未构建
-    aria2c -x 16 -s 16 http://seafile.jyx2048.com:2345/waydroid-runner/sway-for-deepin23-beta1/sway-launcher-icon_1.0.0_all.deb -d /tmp -o sway-desktop-icon.deb
+    aria2c -x 16 -s 16 http://sway.waydroid-runner.gfdgdxi.top/sway-launcher-icon_1.0.0_all.deb -d /tmp -o sway-desktop-icon.deb
     sudo apt install /tmp/sway-desktop-icon.deb
     exit
 fi
@@ -22,6 +22,9 @@ if [[ -f /etc/deepin_version ]]; then
         #sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FD6EEA1F20CD4B27
         sudo cp -v /tmp/gfdgd-xi-sources/gpg.asc.gpg /etc/apt/trusted.gpg.d/gfdgdxi-list-waydroid-sway.gpg
         sudo cp -v /tmp/gfdgd-xi-sources/github.list /etc/apt/sources.list.d/gfdgdxi-list-waydroid-sway.list
+        sudo apt update
+        sudo apt install sway -y
+        sudo apt install sway-launcher-icon -y
         exit
     fi
 fi
