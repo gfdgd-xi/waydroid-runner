4ff2a7bbef803b7b28e1a9936c4495b7e5306eff
mozixun <ronforever@outlook.com>
2023-08-16 14:20:26 +0000

    update Runner_tools/Checks/MagiskCheck.py. 新版面具检测脚本,定位到了新Overlay_rw文件夹
    Signed-off-by: mozixun <ronforever@outlook.com>

--- Runner_tools/Checks/MagiskCheck.py
+++ Runner_tools/Checks/MagiskCheck.py
@@ -1,5 +1,5 @@
 import os
 import sys
-if os.path.exists('/var/lib/waydroid/overlay/system/etc/init/magisk.rc') == True and os.path.exists('/var/lib/waydroid/overlay/system/etc/init/magisk') == True:
+if os.path.exists('/var/lib/waydroid/overlay_rw/system/system/etc/init/magisk.rc') == True and os.path.exists('/var/lib/waydroid/overlay_rw/system/system/etc/init/magisk') == True:
     sys.exit(0)
 sys.exit(1)
