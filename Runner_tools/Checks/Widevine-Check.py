ad110f79338ef40be67825be1d4b0013338223c8
mozixun <ronforever@outlook.com>
2023-08-16 14:27:08 +0000

    检测Widevine是否正确安装(x86_64与arm64通用) 这检测比较浅,具体以软件检测为主 不需要sudo以普通用户执行即可
    Signed-off-by: mozixun <ronforever@outlook.com>

---
+++ Runner_tools/Checks/Widevine-Check.py
@@ -0,0 +1,6 @@
+#Python3 Program
+#不需要以root执行!
+import os
+import sys
+if os.path.exists('/var/lib/waydroid/overlay/vendor/etc/vintf/manifest') == True and os.path.exists('/var/lib/waydroid/overlay/vendor/lib/mediadrm') == True and os.path.exists('/var/lib/waydroid/overlay/vendor/lib64/mediadrm') == True:sys.exit(0)
+else:sys.exit(1)
