3179dbf8066bc94f7e0c62a1076937cc4722f542
mozixun <ronforever@outlook.com>
2023-08-17 05:16:00 +0000

    允许应用旋转90度
    Signed-off-by: mozixun <ronforever@outlook.com>

---
+++ Runner_tools/Off-function/Allow-rotate.py
@@ -0,0 +1,16 @@
+#Please start waydroid session before running this Script!
+#Please running by root!
+
+import os
+import sys
+
+a = os.popen('waydroid status').readlines()  #检查运行状态
+if a[0].find('STOPPED')!=-1:
+    print('-请先启动容器Session!')
+    print('-程序出现异常,正在退出')
+    sys.exit(1)
+
+print('-正在设置允许应用旋转屏幕')
+os.system('sudo waydroid shell wm set-fix-to-user-rotation disabled')
+print('-完成!')
+sys.exit(0)
