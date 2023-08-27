ebfd84450fba9eff28fda341239e3fa587eb7369
mozixun <ronforever@outlook.com>
2023-08-27 12:28:12 +0000

    获取android_id的脚本,请用Android shell执行 forked from docs.waydro.id
    Signed-off-by: mozixun <ronforever@outlook.com>

---
+++ Runner_tools/Android-id-reader/gms.sh
@@ -0,0 +1,5 @@
+#Android-Sh Script
+#临时获取GMS的脚本,执行后会删除
+#forked from docs.waydro.id
+
+ANDROID_RUNTIME_ROOT=/apex/com.android.runtime ANDROID_DATA=/data ANDROID_TZDATA_ROOT=/apex/com.android.tzdata ANDROID_I18N_ROOT=/apex/com.android.i18n sqlite3 /data/data/com.google.android.gsf/databases/gservices.db "select * from main where name = \"android_id\";"
