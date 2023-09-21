a28660000d36db8383ee9b6f22bd6c403aa2652e
mozixun <ronforever@outlook.com>
2023-09-21 13:15:45 +0000

    update Runner_tools/Waydroid_Installer/Waydroid-AutoConfig.py.
    Signed-off-by: mozixun <ronforever@outlook.com>

--- Runner_tools/Waydroid_Installer/Waydroid-AutoConfig.py
+++ Runner_tools/Waydroid_Installer/Waydroid-AutoConfig.py
@@ -4,7 +4,6 @@ import os
 import sys
 import subprocess
 programPath = os.path.split(os.path.realpath(__file__))[0]  # 获取程序路径
-# os.chdir(os.path.split(os.path.realpath(__file__))[0])  #定位到当前运行目录
 
 print('-请在下方输入您的sudo用户密码:')
 os.system('sudo echo 提权完成! && clear')
