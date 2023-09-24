4c82cb4541b46c5e2c5ae21d2c1ed6fac15f2c18
mozixun <ronforever@outlook.com>
2023-09-24 09:18:33 +0000

    update Runner_tools/Waydroid_Installer/Waydroid-AutoConfig.py. 重修不识别运行状态的bug(没办法就这么眼瞎)
    Signed-off-by: mozixun <ronforever@outlook.com>

--- Runner_tools/Waydroid_Installer/Waydroid-AutoConfig.py
+++ Runner_tools/Waydroid_Installer/Waydroid-AutoConfig.py
@@ -58,9 +58,9 @@ else:print('失败,请自行排查问题!')
 
 print('-正在启动Waydroid Session,耗时会比较长,请耐心等待(一般不超过6分钟)')
 print('-正在等待启动Waydroid Session:',end='')  ###启动Waydroid Session
-session=subprocess.Popen(['ping','baidu.com'],stdout=subprocess.PIPE,shell=True)
+session=subprocess.Popen(['waydroid','session','start'],stdout=subprocess.PIPE,shell=True)
 while True:         #循环检测Waydroid session是否已启动
-    WaydroidStatus = str(session.stdout.readline())
+    WaydroidStatus = str(session.stdout.read())
     if WaydroidStatus.find('is ready') != -1:    ###检测session已经启动
         print('已检测启动!')
         break
