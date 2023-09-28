156d4cac65da7cf07856eb7563e92292a75faf09
mozixun <ronforever@outlook.com>
2023-09-28 13:56:30 +0000

    update Runner_tools/SystemConfigs/Clipboard-enable.py. 直接强开pip,支不支持管他呢
    Signed-off-by: mozixun <ronforever@outlook.com>

--- Runner_tools/SystemConfigs/Clipboard-enable.py
+++ Runner_tools/SystemConfigs/Clipboard-enable.py
@@ -27,11 +27,7 @@ if os.system('sudo apt update && sudo apt install wl-clipboard -y')!=0:
     Running_err()
 
 print('\n-正在添加pyclip(pip3包)')
-if sys_name.find('Ubuntu')!=-1 and (sys_name.find('lunar')!=-1 or sys_name.find('mantic')!=-1):    #Ubuntu 23.04和23.10及以上版本要特殊处理
-    print('-检测到Ubuntu 23.04+,正在执行强制安装')
-    os.system('python3 -m pip install pyclip -i https://pypi.tuna.tsinghua.edu.cn/simple  --break-system-packages')
-else:
-    os.system('python3 -m pip install pyclip -i https://pypi.tuna.tsinghua.edu.cn/simple')
+os.system('python3 -m pip install pyclip -i https://pypi.tuna.tsinghua.edu.cn/simple  --break-system-packages')
 
 print('\n-添加成功,程序运行完成!重启电脑后剪切板互通功能生效!')
 sys.exit(0)
